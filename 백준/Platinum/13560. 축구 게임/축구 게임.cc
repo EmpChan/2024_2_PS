6676efed1d295e28f13900e96cb53715c499a030
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-07-20 14:32:01 +0900

    [Platinum V] Title: 축구 게임, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Platinum/13560. 축구 게임/축구 게임.cc
+++ 백준/Platinum/13560. 축구 게임/축구 게임.cc
@@ -3,29 +3,39 @@
 
 using namespace std;
 
-int arr[10000];
+int arr[10000], n;
+
+bool is_valid(int s){
+    if(s!=n*(n-1)/2)return 0;
+    int total = 0;
+    for(int i=0; i<n; i++){
+        total+=arr[i];
+        if(total < i*(i+1) / 2)return 0;
+    }
+
+    return 1;
+}
 
 int main() {
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0); 
 
-    int n,r, s =0;
+    int t, s= 0;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    for(int i=0; i<n; i++){
+        cin >> t;
+        s+=t;
+        arr[i] = t;
     }
-    sort(arr, arr + n);
-    for (int i = 0; i < n; i++) {
-        s += arr[i];
-        if (s >= i * (i + 1) / 2) {
-            continue;
-        }
-        else {
-            cout << -1;
-            return 0;
-        }
+    
+    sort(arr,arr+n);
+
+    if(is_valid(s)){
+        cout << 1;
     }
-    if (s != n * (n - 1) / 2)cout << -1;
-    else cout << 1;
+    else{
+        cout << -1;
+    }
     
     return 0;
 }
