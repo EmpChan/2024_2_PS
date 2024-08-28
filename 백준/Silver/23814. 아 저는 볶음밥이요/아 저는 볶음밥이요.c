1438b0c33e26f0d6d338b0afed0b35e433bb8c6b
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-08-28 19:50:23 +0900

    [Silver II] Title: 아 저는 볶음밥이요, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/23814. 아 저는 볶음밥이요/아 저는 볶음밥이요.c
@@ -0,0 +1,1 @@
+main(){long d,n,m,k,a,l,t;scanf("%ld%ld%ld%ld",&d,&n,&m,&k);n%=d;m%=d;a=k/d,l=k%d;if(n<m){t=n;n=m;m=t;}if(n+l>=d){l-=d-n;if(m+l>=d)l-=d-m;printf("%ld",a*d+l);}else if(a>0){l+=d;a--;if(n+m+l>=2*d)l-=d-n+d-m;printf("%ld",a*d+l);}else printf("%ld",k);}
