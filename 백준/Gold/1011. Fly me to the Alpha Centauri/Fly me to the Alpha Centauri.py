e6d159def74de361db7e3e08aac076ed7e3dc16c
JaeChanHwang <hss598876@gmail.com>
2024-09-14 16:03:32 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 360 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,18 @@
+def sol():
+    a,b=map(int,input().split())
+    d = b-a
+    now = 0
+    w = 0
+    cnt = 0
+    while(now < d):
+        if(now + w+1 >= d):
+            cnt+=1
+            break
+        cnt+=2
+        w+=1
+        now+= w*2
+
+    print(cnt)
+
+for i in [0]*int(input()):
+    sol()
