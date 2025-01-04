de61cc8583aa126460ca043266748c19bb934a6c
JaeChanHwang <emperor0827@gmail.com>
2025-01-04 09:41:25 +0900

    [Silver III] Title: queuestack, Time: 40 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.cc
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int sq[100001];
-int da[100001];
-queue<int>q;
+vector<int>qst;
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n,cnt=0;
+    deque<int>dq;
+    int n, tmp, m;
     cin >> n;
+    qst.resize(n);
+    for(int i=0; i<n; i++)cin >> qst[i];
     for(int i=0; i<n; i++){
-        cin >> sq[i];
-    }
-    for(int i=0; i<n; i++){
-        cin >> da[i];
-    }
-    for(int i=n-1; i>=0; i--){
-        if(sq[i])continue;
-        q.push(da[i]);
-    }
-    int m;
-    cin >> m;
-    for(int j=0; j<m; j++){
-        int tmp;
         cin >> tmp;
-        q.push(tmp);
-        cout << q.front() << ' ';
-        q.pop();
+        if(!qst[i])dq.push_front(tmp);
+    }
+    cin >> m;
+    for(int i=0; i<m; i++){
+        cin >> tmp;
+        dq.push_back(tmp);
+        cout << dq.front() << ' ';
+        dq.pop_front();
     }
 
     return 0;
-}
+}               
