25a26fa26e308ce1bbb157810bd5fee1eaf4727f
JaeChanHwang <emperor0827@gmail.com>
2025-01-20 22:22:23 +0900

    [Silver V] Title: 단어 정렬, Time: 28 ms, Memory: 3108 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,72 +1,74 @@
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
 
-using namespace std;
+char arr[20000][51];
+char tmp[20000][51];
 
-const int MAX_SIZE = 20001;
-
-string temp[MAX_SIZE];
-string str[MAX_SIZE];
-
-int compare(string a, string b)
-{
-	if (a.length() < b.length())
-		return 1;
-	else if (a.length() == b.length())
-	{
-		if (a <= b)
-			return 1;
-		else
-			return 0;
-	}
-	else
-		return 0;
+int comlen(char a[], char b[]) {
+    if (strlen(a) < strlen(b)) {
+        return 1;
+    }
+    else if (strlen(a) > strlen(b)) {
+        return 2;
+    }
+    else
+        return 3;
 }
 
-void merge(int s, int e)
-{
-	int mid = (s + e) / 2;
-	int i = s, j = mid + 1, k = s;
-	while (i <= mid && j <= e)
-	{
-		if (compare(str[i], str[j]))
-			temp[k++] = str[i++];
-		else
-			temp[k++] = str[j++];
-	}
-	while (i <= mid) temp[k++] = str[i++];
-	while (j <= e) temp[k++] = str[j++];
-	for (int m = s; m <= e; m++) str[m] = temp[m];
-
+int combook(char a[], char b[]) {
+    if (strcmp(a, b) <= 0)
+        return 1;
+    else
+        return 2;
 }
 
-void partition(int s, int e)
-{
-	if (s >= e)
-		return;
-	int mid = (s + e) / 2;
-	partition(s, mid);
-	partition(mid + 1, e);
-	merge(s, e);
+void merge(int left, int right) {
+    int mid = (left + right) / 2;
+    int lp = left, rp = mid + 1, k = left;
+    while ((lp <= mid) && (rp <= right)) {
+        if (comlen(arr[lp], arr[rp]) == 1) {
+            strcpy(tmp[k++], arr[lp++]);
+        }
+        else if (comlen(arr[lp], arr[rp]) == 2) {
+            strcpy(tmp[k++], arr[rp++]);
+        }
+        else {
+            if (combook(arr[lp], arr[rp]) == 1)
+                strcpy(tmp[k++], arr[lp++]);
+            else
+                strcpy(tmp[k++], arr[rp++]);
+        }
+    }
+    while (lp <= mid) {
+        strcpy(tmp[k++], arr[lp++]);
+    }
+    while (rp <= right) {
+        strcpy(tmp[k++], arr[rp++]);
+    }
+    for (int i = left; i <= right; i++)strcpy(arr[i], tmp[i]);
 }
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> str[i];
-	partition(0, n - 1);
-	for (int i = 0; i < n; i++)
-	{
-		if (i == 0) cout << str[i] << '\n';
-		else if (str[i] == str[i - 1])
-			continue;
-		else
-			cout << str[i] << '\n';
-	}
-	return 0;
+void partition(int left, int right) {
+    if (left >= right) return;
+    int mid = (left + right) / 2;
+    partition(left, mid);
+    partition(mid + 1, right);
+    merge(left, right);
+}
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+    partition(0, n - 1);
+    for (int i = 0; i < n; i++) {
+        while(i + 1<n && strcmp(arr[i], arr[i+1]) == 0)i++;
+        printf("%s\n", arr[i]);
+    }
+    return 0;
 }
